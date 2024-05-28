0c00b90c426ef1aadf208762d874b66174f6a704
Cheikh MARA <cheikhmara@gmail.com>
2024-05-28 11:24:04 +0000

    Maj Pdi.js
    Ajout des codes couleurs de fond sur la colonne note du tableau des notes de l'élève dans le composant Pdi.js

--- src/Components/Pdi/Trow3.js
+++ src/Components/Pdi/Trow3.js
@@ -2,13 +2,24 @@ import React from 'react'
 //import { Link } from 'react-router-dom';
 
 const Trow3 = ({id, enseignant, matiere, note}) => {
+  let bgcolor = '';
+  if(note < 30)
+    bgcolor = '#FF0000';
+  else if(note >= 30 && note < 50)
+    bgcolor = '#FF6D01';
+  else if(note >= 50 && note < 70)
+    bgcolor = '#FFFF00';
+  else if(note >= 70 && note < 90)
+    bgcolor = '#00FF00';
+  else if(note >= 90 && note <= 100)
+    bgcolor = '#38761D';
 
   return (
     <tr indice={id} id={"row"+id}>
       {/*<td>{id}</td>
       <td>{enseignant}</td>*/}
       <td>{matiere}</td>
-      <td>{note}</td>
+      <td style={{ backgroundColor: bgcolor }}>{note}</td>
       <td>
         {/*<Link  
           className="" 
